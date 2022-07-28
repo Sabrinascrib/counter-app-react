8b76f316951606143a1e0a9a681badf9e4768667
sabrina <sabrinaabrodi20@gmail.com>
2022-07-28 08:22:12 +0200

    styling the buttons

--- src/App.js
+++ src/App.js
@@ -18,23 +18,23 @@ function App() {
   };
 
   return (
-    <div className="App flex bg-white w-96 h-96 items-center flex-col mt-52">
-      <span>{count}</span>
+    <div className="App flex bg-white w-96 h-96 items-center flex-col mt-52 mx-auto">
+      <span className="mt-9 text-2xl">{count}</span>
       <div className="buttons mt-5">
         <button
-          className="p-2 bg-blue-200 w-24 mr-8 rounded-md"
+          className=" text-2xl shadow p-2 bg-blue-200 w-24 mr-8 rounded-md"
           onClick={decrease}
         >
           -
         </button>
         <button
-          className="p-1 border-solid border-4 border-blue-200 w-24 rounded-md"
+          className=" text-2xl shadow p-1 border-solid border-4 border-blue-200 w-24 rounded-md"
           onClick={setToZero}
         >
-          Reset
+          reset
         </button>
         <button
-          className="p-2 bg-blue-200 w-24 ml-8 rounded-md"
+          className=" text-2xl shadow p-2 bg-blue-200 w-24 ml-8 rounded-md"
           onClick={increase}
         >
           +
